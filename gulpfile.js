7c68fa1b5f96c20b866258038bca9df414a8b692
Dennis Baklan <dennis.baklan@gmail.com>
2019-11-01 13:23:36 +0200

    New Gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,113 +1,123 @@
-const gulp = require('gulp'); //Gulp
-const sass = require('gulp-sass');  //sass
-const browserSync =  require('browser-sync').create();//живая перезагрузка после изменения файла(ов)
-const uglify = require('gulp-uglify');//минификация js
-const cleanCSS = require('gulp-clean-css');//минификация css
-const clean = require('gulp-clean');// чистка файлов перед buildом
-const concat = require('gulp-concat');//объединяет несколько файлов в один
-const sourcemaps = require('gulp-sourcemaps'); //карта источников файла после их конкатенации, чтобы легче править код.
-const imagemin = require('gulp-imagemin'); ////оптимизация изображений
-const autoprefixer = require('gulp-autoprefixer'); //обеспечивают поддержку браузерами CSS3 свойств и автоматически ее проставляет
+const gulp = require('gulp');
+const sass = require('gulp-sass');							//sass
+const browserSync = require('browser-sync').create();		//runtime watcher and changer
+const clean = require('gulp-clean');						//cleaner product directory "dev"
+const cleanCSS = require('gulp-clean-css');					//CSS minifier
+const rigger = require('gulp-rigger');                      //включение кусков html в другой
+const rename = require("gulp-rename");						//rename files after minify
+const concat = require('gulp-concat');						//concat for js
+const terser = require('gulp-terser');						//minify for js
+const autoprefixer = require('gulp-autoprefixer');			//cross-browser compatibility css
+const imagemin = require('gulp-imagemin');                  // mini images
+
+const fontsFiles = [					
+   		//составляем массив переменних с все файлов шрифтов, для переноса в папку разработки
+	'./app/fonts/**.woff',
+];
+
+const imgFiles = [
+    
+    './app/img/**/**.jpg',
+    './app/img/**/**.png'
+];
+
+const paths = {
+    html: {
+        
+        src: './app/templates/*.html',
+        build: './dist/'
+    },
+    styles: {
+        src: './app/scss/**/*.scss',
+        build: './dist/css/'
+    },
+    scripts: {
+        src: './app/js/**/*.js',
+        build: './dist/js/'
+    },
+    img: {
+        src: './app/img/*',
+        build: './dist/img/'
+    }
+};
 
 
-
-
-// const path = {
-//     app: {
-//         html:   'src/index.html',
-//         js:     'src/js/**/*.js',
-//         styles: 'src/scss/**/*.scss',
-//         img:    'src/img/**/*.*',
-//     },
-//     dist: {
-//         html:   'dist',
-//         js:     'dist/js',
-//         styles: 'dist/css',
-//         img:    'dist/images',
-//     },
-//     clean: 'dist/*'
-// }
-
-gulp.task('clean', function(){                          //очистка папки dist
-    return gulp.src('/dist', {read: false})
+function cleandev() {										//модуль отчистки папки перед каждой расспаковкой
+    return gulp.src('./dist', {read: false})
         .pipe(clean());
-});
+};
 
-gulp.task('html', function () {
-    return gulp.src('./app/index.html')
-    .pipe(gulp.dest('./dist'))
-    .pipe(browserSync.stream());
-});
-
-
-
-gulp.task('sass', function () {                     //компиляция scss файлов в css
-    return gulp.src('./app/scss/*.scss')
-        // .pipe(sourcemaps.init())
-        .pipe(sass())  //из scss в .css
-        .pipe(autoprefixer({                        //добавление вендорных префиксов к CSS свойствам для поддержки последних нескольких версий каждого из браузеров
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
-        // .pipe(cleanCSS())             //минификация css
-        .pipe(concat('style.min.css'))  //конкатенация
-        // .pipe(sourcemaps.write())
-        .pipe(gulp.dest('./dist/css'))  
-        .pipe(browserSync.stream());
-});
-
-gulp.task('uglify', function () {
-    gulp.src('./app/js/**/*.js')
-    //   .pipe(concat('main.js'))
-      .pipe(uglify())
-      .pipe(gulp.dest('./dist/js'))
-      .pipe(browserSync.stream());
-}); // объединяем файлы JS в один, минифицируем, записываем в dist
-
-gulp.task('imagemin', function () {  //минифицирует изображения
-    return gulp.src('./app/img/*')
-        .pipe(imagemin({
-            interlaced: true,
-            progressive: true,
-            svgoPlugins: [{
-                removeViewBox: false
-            }]
-        }))
+function img() {											//модуль переноса картинок
+    return gulp.src(imgFiles)
+    .pipe(imagemin())
         .pipe(gulp.dest('./dist/img'))
+}
+
+function fonts () {											//Copy fonts to dir "dev"
+    return gulp.src(fontsFiles)
+        .pipe(gulp.dest('./dist/fonts'))
+}
+
+function js () {											//Copy fonts to dir "dev"
+    return gulp.src('./app/js/*.js')
+        .pipe(gulp.dest('./dist/js'))
+}
+
+function scripts () {
+    
+    return gulp.src('./app/js/**/*.js')
+        .pipe(terser({											//terser
+			toplevel: true
+		}))														//minify js
+        .pipe(concat('all.js'))									//concat all js files
+		.pipe(rename(function (path) {							// function of rename extname for .css
+            path.extname = ".min.js";
+        }))
+        .pipe(gulp.dest('./dist/js'))
+		.pipe(browserSync.stream());
+}
+
+function html() {
+    return gulp.src("./app/templates/index.html")
+        .pipe(rigger())
+        .pipe(gulp.dest('./dist/'))
         .pipe(browserSync.stream());
-});
-
-gulp.task('watch', function(){
-    gulp.watch('app/scss/**/*.*', gulp.series('sass'));
-    gulp.watch('app/img/**/*.*', gulp.series('imagemin'));
-    gulp.watch('app/js/**/*.js', gulp.series('uglify'));
-    gulp.watch('app/*.html', gulp.series('html'));
-});
+}
 
 
+function forSass() {
+    return gulp.src('./app/scss/*.scss')
+        .pipe(sass())
+        .pipe(cleanCSS({level: 2}))								// minifyCSS
+        .pipe(autoprefixer({
+            browsers: ['> 0.1%'],								// для браузеров которые использует 0.1%
+			cascade: false
+        }))
+        .pipe(rename(function (path) {							// function of rename extname for .css
+            path.extname = ".min.css";
+        }))
+        .pipe(gulp.dest('./dist/css'))
+		.pipe(browserSync.stream());
+}
 
+function watch() {
+	browserSync.init({											// инструмент для live reload
+		server: {
+			baseDir: "./"
+		}
+	});
 
-gulp.task('serve', function () {
-    browserSync.init({
-        
-            server: {
-                baseDir: "app/"
-            }
-        })
-        
-    })                                  //запускаем статический сервер и на нем содержимое папки 'dist'
-    browserSync.watch('dist/**/*.*').on('change', browserSync.reload); //броузерсинковский вотчер следит за файлами 'dist/**/*.*' и при событии 'change' вызывает функцию reload, которой в качестве аргумента будет передан путь к изменненному файлу
+	gulp.watch('./app/**/*.scss', forSass);				// ставим watcher для слежения за изменениями в файлах
+	gulp.watch('./app/**/*.js', scripts);
+}
 
-
-
-
-// gulp.task('build', gulp.series('clean', gulp.series(sass, uglify, imagemin)));
-// gulp.task('dev', gulp.series('build', watch));
-
-
-gulp.task('default', gulp.series('serve', function(){
-    console.log('=== ALL DONE ===')
-  }));
-  
-  
-
+gulp.task('cleandev', cleandev);
+gulp.task('img', img);
+gulp.task('scripts', scripts);
+gulp.task('sass', forSass);
+gulp.task('html', html);
+gulp.task('watch', watch);
+gulp.task('fonts', fonts);
+gulp.task('js', js);
+gulp.task('build', gulp.series('cleandev', gulp.parallel(img, fonts, scripts, html, forSass)));
+gulp.task('dev', gulp.series('build', watch));
